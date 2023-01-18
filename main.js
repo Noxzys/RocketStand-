7d10c2a0bfa429ea69837df07b0ea8fea94a60e0
William Oliveira <105165419+Noxzys@users.noreply.github.com>
2023-01-18 00:28:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,51 @@
+window.addEventListener("scroll", onScroll);
+
+onScroll();
+
+function onScroll() {
+  showBackToTopButtonOnScroll();
+  showNavOnScroll();
+}
+
+function showNavOnScroll() {
+  let nav = document.getElementById("navigation");
+
+  if (scrollY === 0) {
+    nav.classList.remove("scroll");
+  } else {
+    nav.classList.add("scroll");
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  let nav = document.getElementById("backToTopButton");
+
+  if (scrollY < 550) {
+    backToTopButton.classList.add("scroll");
+  } else {
+    backToTopButton.classList.remove("scroll");
+  }
+}
+
+function openMenu() {
+  document.body.classList.add("menu-expanded");
+}
+
+function closeMenu() {
+  document.body.classList.remove("menu-expanded");
+}
+
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`
+#home, 
+#home img, 
+#home .stats, 
+#services,
+#services header,
+#services .card,
+#price,
+#price header,
+#price .content`);
